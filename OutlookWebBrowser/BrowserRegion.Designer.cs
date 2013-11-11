175f6579ce29f39a11380bffbf48eb34c97aebbe
arleyschrock <arleyschrock@gmail.com>
2013-11-11 12:32:10 -0600

    Initial commit

---
+++ OutlookWebBrowser/BrowserRegion.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace OutlookWebBrowser
+{
+    [System.ComponentModel.ToolboxItemAttribute(false)]
+    partial class BrowserRegion : Microsoft.Office.Tools.Outlook.FormRegionBase
+    {
+        public BrowserRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            : base(Globals.Factory, formRegion)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Awesomium.Core.WebPreferences webPreferences2 = new Awesomium.Core.WebPreferences(true);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.addressBox1 = new Awesomium.Windows.Forms.AddressBox();
+            this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.webSessionProvider1 = new Awesomium.Windows.Forms.WebSessionProvider(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.addressBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(534, 22);
+            this.panel1.TabIndex = 0;
+            // 
+            // addressBox1
+            // 
+            this.addressBox1.AcceptsReturn = true;
+            this.addressBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.addressBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.addressBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addressBox1.Location = new System.Drawing.Point(0, 0);
+            this.addressBox1.Name = "addressBox1";
+            this.addressBox1.Size = new System.Drawing.Size(534, 20);
+            this.addressBox1.TabIndex = 0;
+            this.addressBox1.URL = new System.Uri("http://www.bing.com", System.UriKind.Absolute);
+            this.addressBox1.WebControl = this.webControl1;
+            // 
+            // webControl1
+            // 
+            this.webControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webControl1.Location = new System.Drawing.Point(0, 22);
+            this.webControl1.Size = new System.Drawing.Size(534, 180);
+            this.webControl1.Source = new System.Uri("http://www.bing.com", System.UriKind.Absolute);
+            this.webControl1.TabIndex = 1;
+            // 
+            // webSessionProvider1
+            // 
+            webPreferences2.CanScriptsAccessClipboard = true;
+            webPreferences2.EnableGPUAcceleration = true;
+            webPreferences2.FileAccessFromFileURL = true;
+            webPreferences2.SmoothScrolling = true;
+            webPreferences2.UniversalAccessFromFileURL = true;
+            webPreferences2.WebGL = true;
+            this.webSessionProvider1.Preferences = webPreferences2;
+            this.webSessionProvider1.Views.Add(this.webControl1);
+            // 
+            // BrowserRegion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "BrowserRegion";
+            this.Size = new System.Drawing.Size(534, 202);
+            this.FormRegionShowing += new System.EventHandler(this.BrowserRegion_FormRegionShowing);
+            this.FormRegionClosed += new System.EventHandler(this.BrowserRegion_FormRegionClosed);
+            this.Load += new System.EventHandler(this.BrowserRegion_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        #region Form Region Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private static void InitializeManifest(Microsoft.Office.Tools.Outlook.FormRegionManifest manifest, Microsoft.Office.Tools.Outlook.Factory factory)
+        {
+            manifest.FormRegionName = "WebBrowser";
+            manifest.ShowReadingPane = false;
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Awesomium.Windows.Forms.AddressBox addressBox1;
+        private Awesomium.Windows.Forms.WebControl webControl1;
+        private Awesomium.Windows.Forms.WebSessionProvider webSessionProvider1;
+
+        public partial class BrowserRegionFactory : Microsoft.Office.Tools.Outlook.IFormRegionFactory
+        {
+            public event Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler FormRegionInitializing;
+
+            private Microsoft.Office.Tools.Outlook.FormRegionManifest _Manifest;
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public BrowserRegionFactory()
+            {
+                this._Manifest = Globals.Factory.CreateFormRegionManifest();
+                BrowserRegion.InitializeManifest(this._Manifest, Globals.Factory);
+                this.FormRegionInitializing += new Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler(this.BrowserRegionFactory_FormRegionInitializing);
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public Microsoft.Office.Tools.Outlook.FormRegionManifest Manifest
+            {
+                get
+                {
+                    return this._Manifest;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.IFormRegion Microsoft.Office.Tools.Outlook.IFormRegionFactory.CreateFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            {
+                BrowserRegion form = new BrowserRegion(formRegion);
+                form.Factory = this;
+                return form;
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            byte[] Microsoft.Office.Tools.Outlook.IFormRegionFactory.GetFormRegionStorage(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                throw new System.NotSupportedException();
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            bool Microsoft.Office.Tools.Outlook.IFormRegionFactory.IsDisplayedForItem(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                if (this.FormRegionInitializing != null)
+                {
+                    Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs cancelArgs = Globals.Factory.CreateFormRegionInitializingEventArgs(outlookItem, formRegionMode, formRegionSize, false);
+                    this.FormRegionInitializing(this, cancelArgs);
+                    return !cancelArgs.Cancel;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.FormRegionKindConstants Microsoft.Office.Tools.Outlook.IFormRegionFactory.Kind
+            {
+                get
+                {
+                    return Microsoft.Office.Tools.Outlook.FormRegionKindConstants.WindowsForms;
+                }
+            }
+        }
+    }
+
+    partial class WindowFormRegionCollection
+    {
+        internal BrowserRegion BrowserRegion
+        {
+            get
+            {
+                foreach (var item in this)
+                {
+                    if (item.GetType() == typeof(BrowserRegion))
+                        return (BrowserRegion)item;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ OutlookWebBrowser/BrowserRegion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookWebBrowser
+{
+    partial class BrowserRegion
+    {
+        #region Form Region Factory
+
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Appointment)]
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Contact)]
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Activity)]
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Note)]
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Post)]
+        [Microsoft.Office.Tools.Outlook.FormRegionMessageClass(Microsoft.Office.Tools.Outlook.FormRegionMessageClassAttribute.Task)]
+        [Microsoft.Office.Tools.Outlook.FormRegionName("OutlookWebBrowser.BrowserRegion")]
+        public partial class BrowserRegionFactory
+        {
+            // Occurs before the form region is initialized.
+            // To prevent the form region from appearing, set e.Cancel to true.
+            // Use e.OutlookItem to get a reference to the current Outlook item.
+            private void BrowserRegionFactory_FormRegionInitializing(object sender, Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs e)
+            {
+            }
+        }
+
+        #endregion
+
+        // Occurs before the form region is displayed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void BrowserRegion_FormRegionShowing(object sender, System.EventArgs e)
+        {
+        }
+
+        // Occurs when the form region is closed.
+        // Use this.OutlookItem to get a reference to the current Outlook item.
+        // Use this.OutlookFormRegion to get a reference to the form region.
+        private void BrowserRegion_FormRegionClosed(object sender, System.EventArgs e)
+        {
+        }
+
+        private void BrowserRegion_Load(object sender, EventArgs e)
+        {
+            var docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var dataPath = Path.Combine(docPath, "Outlook Web Browser");
+            webSessionProvider1.DataPath = dataPath;
+        }
+    }
+}
